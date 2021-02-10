5c6d5e5e38006cb4556ab316297f42867b5d1108
Zach Leatherman <zachleatherman@gmail.com>
2021-02-10 17:41:45 -0600

    Eureka

--- netlify/functions/eleventy/eleventy.js
+++ netlify/functions/eleventy/eleventy.js
@@ -10,19 +10,19 @@ exports.handler = async (event, context) => {
     process.env.ELEVENTY_CLOUD = process.env.DEPLOY_PRIME_URL || "";
     console.log( "Dir", process.cwd() );
     console.log( "test.njk exists", fs.existsSync("./test.njk") );
-    let elev = new Eleventy("./test.njk");
+    let elev = new Eleventy("./src/netlify/functions/test.njk");
     await elev.init();
     console.log( "Globs", elev.eleventyFiles.getFileGlobs() );
     console.log( "Files", await elev.eleventyFiles.getFiles() );
 
-    fs.readdir(process.cwd() + "/src/netlify/functions/eleventy", function (err, files) {
+    fs.readdir(process.cwd() + "/src/netlify/functions", function (err, files) {
       if (err) {
         console.log('Unable to scan directory: ' + err);
         return
       } 
 
       files.forEach(function (file) {
-        console.log("File in /src/netlify/functions/eleventy:", file); 
+        console.log("File in /src/netlify/functions:", file); 
       });
     });
 
