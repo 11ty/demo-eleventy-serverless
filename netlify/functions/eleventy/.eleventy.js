7032333e25071c32a91de7b8abe73a7180ed0bae
Zach Leatherman <zachleatherman@gmail.com>
2021-03-24 21:09:03 -0500

    Deleting things to move it up to root

--- netlify/functions/eleventy/.eleventy.js
+++
@@ -1,7 +0,0 @@
-const eleventyVue = require("@11ty/eleventy-plugin-vue");
-
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(eleventyVue, {
-    cacheDirectory: "/tmp/.cache/vue/"
-  });
-};
