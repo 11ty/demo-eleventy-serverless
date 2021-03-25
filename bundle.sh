1057641b4d6c8362a6939dda655196ed3bce006f
Zach Leatherman <zachleatherman@gmail.com>
2021-03-25 09:04:54 -0500

    Try again

--- bundle.sh
+++ bundle.sh
@@ -1,9 +1,10 @@
 echo "Bundling the Eleventy Cloud Netlify Function"
 
+# Recursively make directories
 mkdir -p ./netlify/functions/cloud/src/
 
 # Move Netlify Function definition
-# cp -v ./netlify/cloud.js ./netlify/functions/cloud/index.js
+cp -v ./netlify/functions/cloud.js ./netlify/functions/cloud/index.js
 
 # Config
 cp -v .eleventy.js ./netlify/functions/cloud/
