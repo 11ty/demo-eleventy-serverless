788f69d4e13d2cb08c80cbc3bc76147e7e4d46bf
Zach Leatherman <zachleatherman@gmail.com>
2021-03-25 09:02:42 -0500

    Don’t copy the js it already exists

--- bundle.sh
+++ bundle.sh
@@ -3,7 +3,7 @@ echo "Bundling the Eleventy Cloud Netlify Function"
 mkdir -p ./netlify/functions/cloud/src/
 
 # Move Netlify Function definition
-cp -v ./netlify/cloud.js ./netlify/functions/cloud/index.js
+# cp -v ./netlify/cloud.js ./netlify/functions/cloud/index.js
 
 # Config
 cp -v .eleventy.js ./netlify/functions/cloud/
