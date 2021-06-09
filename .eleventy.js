ed846c26b02d2cae819e8380b584d5ed6f5b8ae8
Zach Leatherman <zachleatherman@gmail.com>
2021-06-09 08:44:03 -0500

    Updates to use new Eleventy Serverless code.

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,24 @@
+const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
+
 module.exports = function(eleventyConfig) {
-  return {
-    dir: {
-      input: "src"
-    }
-  };
+	eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+		name: "serverless",
+		functionsDir: "./netlify/functions/",
+		copy: [
+			// Advanced options:
+
+			// Self generated collections for re-use
+			// (Check netlify/functions/serverless/index.js for injection)
+			// "_generated-serverless-collections.json",
+
+			// Save build-time data cache (no run-time data requests!)
+			// { from: ".cache/eleventy-cache-assets/", to: "cache" },
+		]
+	});
+
+	return {
+		dir: {
+			input: "src"
+		}
+	};
 };
