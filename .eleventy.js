b3095d8e08f109011feb2452e6b6364bbcebaf67
Zach Leatherman <zachleatherman@gmail.com>
2021-06-09 10:05:24 -0500

    Demo of two serverless plugins side-by-side

--- .eleventy.js
+++ .eleventy.js
@@ -4,16 +4,11 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
 		name: "serverless",
 		functionsDir: "./netlify/functions/",
-		copy: [
-			// Advanced options:
+	});
 
-			// Self generated collections for re-use
-			// (Check netlify/functions/serverless/index.js for injection)
-			// "_generated-serverless-collections.json",
-
-			// Save build-time data cache (no run-time data requests!)
-			// { from: ".cache/eleventy-cache-assets/", to: "cache" },
-		]
+	eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+		name: "ondemandbuilder",
+		functionsDir: "./netlify/functions/",
 	});
 
 	return {
